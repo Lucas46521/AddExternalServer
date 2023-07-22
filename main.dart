5a970119457970d0ca51f9537702e84ee51c6483
Lucas46521 <111234099+Lucas46521@users.noreply.github.com>
2023-07-22 17:15:55 -0300

    .Update main.dart

--- main.dart
+++ main.dart
@@ -45,7 +45,7 @@ void copyText(String copyText) {
 }
 
 String generateUrl(String name, String ip, String port) {
-  return "https://lucas46521.github.io/AddExternalServer/?name=${name}&ip=${ip}&port=${port}";
+  return "https://suinua.github.io/AddExternalServer/?name=${name}&ip=${ip}&port=${port}";
 }
 
 
